0aeef2849d8d37914bb856092fb2d2aa406144ef
DarkBrungardt <kuchamps2020@gmail.com>
2023-07-23 12:54:34 -0500

    connected recent posts to redux

--- src/components/recentPost.js
+++ src/components/recentPost.js
@@ -1,21 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class SearchBar extends Component {
- render() {
+import { connect } from 'react-redux';
+
+import * as actions from '../actions'
+
+class RecentPosts extends Component {
+
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
+
+
+  render() {
     return (
-        <div className="recent-posts">
-            <div className="recent-posts__wrapper">
-                <div className="recent-post__heading">Recent Posts</div>
-                <ul className="recent-post__posts">
-                    <li>recent post 0</li>
-                    <li>recent post 1</li>
-                    <li>recent post 2</li>
-                </ul>
-            </div>
-
+      <div className="recent-posts">
+        <div className="recent-posts__wrapper">
+          <div className="recent-posts__heading">Recent Posts</div>
+          <ul className="recent-posts__posts">
+            <li>recent post 0</li>
+            <li>recent post 1</li>
+            <li>recent post 2</li>
+          </ul>
         </div>
+      </div>
     )
- }
+  }
 }
 
-export default SearchBar;
+export default connect(null, actions) (RecentPosts);
