6620820b6aeeac659063c159e5615ee964432ad8
DarkBrungardt <kuchamps2020@gmail.com>
2023-10-26 17:06:38 -0500

    used axios to perform request

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -12,8 +12,11 @@ export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
             
-            return [...state, recentPost: action.payload];
-    
+        //const recentPost = action.payload;
+            return {
+                ...state, 
+                recentPost
+            }
         default:
             return state;
     }
